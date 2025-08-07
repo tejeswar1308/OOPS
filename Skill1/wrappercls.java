58d2e9bc221ae1e17015003d464e50657ebdcbda
D. Teja <tejadarapu9@gmail.com>
2025-08-07 14:47:24 +0530

    Skill 1 Problems

---
+++ Skill1/wrappercls.java
@@ -0,0 +1,12 @@
+package Skill1;
+
+public class wrappercls {
+	public static void main(String args[]) {
+		int a = 20;
+		Integer a1 = 3;
+		int i = a1.intValue();
+		int j = a1;
+		System.out.println(a1 + " " + i + " " + j);
+	}
+
+}
