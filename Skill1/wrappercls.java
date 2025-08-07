0a5ca49d383506cded9b080f6b750af3c3e94b3e
D. Teja <tejadarapu9@gmail.com>
2025-08-07 14:51:13 +0530

    Update wrappercls.java

--- Skill1/wrappercls.java
+++ Skill1/wrappercls.java
@@ -3,10 +3,11 @@ package Skill1;
 public class wrappercls {
 	public static void main(String args[]) {
 		int a = 20;
-		Integer a1 = 3;
+		Integer a1 = 7;
 		int i = a1.intValue();
 		int j = a1;
 		System.out.println(a1 + " " + i + " " + j);
 	}
+
 
 }
