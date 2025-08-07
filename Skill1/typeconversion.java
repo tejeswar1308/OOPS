34e89a2641b4e10329ec5bcffa449798fd106e76
D. Teja <tejadarapu9@gmail.com>
2025-08-07 14:45:18 +0530

    Add files via upload

---
+++ Skill1/typeconversion.java
@@ -0,0 +1,14 @@
+package Skill1;
+
+public class typeconversion {
+	public static void main(String args[]) {
+		float num = 10.78f;
+		int number = (int)num;
+		long longNum = (long)num;
+		System.out.println(num);
+		System.out.println(number);
+		System.out.println(longNum);
+			
+	}
+
+}
